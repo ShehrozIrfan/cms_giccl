673adfbc2de1e2bea3b05c3076d5744639e445ed
shehrozirfan <shehrozirfan89@gmail.com>
2021-07-03 09:32:56 +0500

    Sending email to user on contact

--- contact.php
+++ contact.php
@@ -1,3 +1,54 @@
+<?php 
+
+$msg = '';
+$msgClass = '';
+
+if(isset($_POST['contact'])) {
+    //getting values
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if(!empty(trim($name)) && !empty(trim($email)) && !empty(trim($message))) {
+
+        //checking for valid email
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $msg = "Please use a valid email";
+            $msgClass = "alert-danger";
+        } else {
+            //send email
+            $toEmail = "shehrozirfan89@gmail.com";
+            $subject = "Contact request from " . $name;
+            $body = '<h2>Contact Request</h2>
+                     <h4>Name: </h4><p>' . $name . '</p>
+                     <h4>Email: </h4><p>' . $email . '</p>
+                     <h4>Message: </h4><p>' . $message . '</p>
+            ';
+
+            //Email headers
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $header .= "Content-type: text/html; charset= UTF-8" . "\r\n";
+
+            //Additional headers
+            $headers .= "From: " . $name . "<" . $email . ">" . "\r\n";
+
+            if(mail($toEmail, $subject, $body, $headers)) {
+                $msg = "Your message has been sent";
+                $msgClass = "alert-success";
+            } else {
+                $msg = "Your message was not sent";
+                $msgClass = "alert-danger";
+            }
+        }
+    } else {
+        //Failed
+        $msg = "Please fill in all fields";
+        $msgClass = "alert-danger";
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +56,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us | GICCL</title>
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- stylesheet -->
     <link rel="stylesheet" href="./assets/styles/style.css">
     <!-- font awesome -->
@@ -17,27 +70,36 @@
 </head>
 <body>
     <!-- header -->
-    <?php include 'header.php' ?> <!-- header ends -->
+    <?php include 'header.php' ?><!-- header ends -->
+
     <!-- contact section -->
     <section id="contact" class="pd_top">
         <div class="container">
             <h2 class="text-center font-weight-bold mt-3 mb-3">Get In Touch</h2>
             <div class="row justify-content-center">
                 <div class="col-md-6">
-                    <form action="" method="post">
+                <?php if($msg != '') { ?>
+                    <div class="alert <?php echo $msgClass ?> text-center alert-dismissible">
+                    <button type = "button" class = "close" data-dismiss = "alert" aria-hidden = "true">
+                        ×
+                    </button>
+                    <?php echo $msg ?>
+                    </div>
+                <?php } ?>
+                    <form action="./contact.php" method="post">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter Name" required>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="<?php echo isset($_POST['name'])? $name : ''; ?>" required>
                           </div>
                         <div class="form-group">
                           <label for="email">Email address</label>
-                          <input type="email" class="form-control" id="email" placeholder="Enter Email" required>
+                          <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" value="<?php echo isset($_POST['email'])? $email : ''; ?>" required>
                         </div>
                         <div class="form-group">
                             <label for="message">Message</label>
-                            <textarea class="form-control" id="message" placeholder="Enter your message" required></textarea>
+                            <textarea class="form-control" id="message" name="message" placeholder="Enter your message" required><?php echo isset($_POST['message'])? $message : ''; ?></textarea>
                           </div>
-                        <button type="submit" class="btn btn-dark">Send Message</button>
+                        <button type="submit" name="contact" class="btn btn-dark">Send Message</button>
                       </form>
                 </div>
             </div>
@@ -57,5 +119,6 @@
     </section><!-- contact section ends -->
     
     <?php include 'footer.php' ?>
+    <script src="./assets/js/app.js"></script>
 </body>
 </html>
